3d6d6272a1b2ce74eed979930201c896ab2242fa
Dima <dimfreelancerdn@gmail.com>
2020-12-21 01:21:50 +0300

    Lesson10 удалил рекламу заменил фон

--- script.js
+++ script.js
@@ -2,7 +2,17 @@
 //Lesson10
 
 
+const adv = document.querySelector('.adv');
+adv.remove();
+// console.log('adv.parentElement: ', adv.parentElement);
+
 
 alert('it works');
 
 
+
+// const body = document.getElementsByTagName('body');
+//меняем фоновую картинку
+document.body.style.backgroundImage = 'url("image/you-dont-know-js.jpg")';
+
+console.log('body: ', document.body.style.backgroundImage);
